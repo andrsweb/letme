d42db4434ac32cbee2a5e1f8b33c81f1ee62b7c5
andrsweb <d3vv.proo@gmail.com>
2024-01-15 19:47:53 +0700

    created bonus page

--- src/js/addDisabled.js
+++ src/js/addDisabled.js
@@ -8,16 +8,20 @@ const addOrRemoveDisabled = () => {
 	const checkboxWrappers = document.querySelectorAll('.checkbox__wrapper')
 
 	checkboxWrappers.forEach((checkboxWrapper) => {
-		const checkbox = checkboxWrapper.querySelector('.checkbox')
 		const labelValue = checkboxWrapper.querySelector('.label__value')
 
-		if (parseInt(labelValue.textContent, 10) === 0) {
-			checkbox.checked = false
-			checkbox.disabled = true
-			checkboxWrapper.classList.add('disabled')
-		} else {
-			checkbox.disabled = false
-			checkboxWrapper.classList.remove('disabled')
+		if (labelValue && labelValue.textContent.trim() !== "") {
+			const checkbox = checkboxWrapper.querySelector('.checkbox')
+
+			if (parseInt(labelValue.textContent, 10) === 0) {
+				checkbox.checked = false
+				checkbox.disabled = true
+				checkboxWrapper.classList.add('disabled')
+			} else {
+				checkbox.disabled = false
+				checkboxWrapper.classList.remove('disabled')
+			}
 		}
 	})
 }
+
