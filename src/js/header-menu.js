b8872cc6d50a6e5546ba4f7a0a89a92bd206227a
andrsweb <d3vv.proo@gmail.com>
2024-01-16 19:20:13 +0700

    Finish project

---
+++ src/js/header-menu.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
+    showHiddenMenu()
+})
+
+const showHiddenMenu = () => {
+    const buttons = document.querySelectorAll('.header__item svg')
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const headerItem = button.closest('.header__item')
+
+            if (headerItem) {
+                headerItem.classList.toggle('opened')
+            }
+        })
+    })
+}
